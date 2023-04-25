7d573971b2dc3b88821195971a9d30032012ccb8
3rickDJ <erickdejesusmolina@gmail.com>
2023-04-24 22:08:10 -0600

    hack for msg and file

--- lib/escitala.rb
+++ lib/escitala.rb
@@ -7,9 +7,26 @@ class Scytale
   def decrypt(msg, key)
     msg.chars.each_slice(key).to_a.transpose.join
   end
+
+  def hack(msg, name='file', dir='files/')
+    valid_size = (2..msg.size).filter { |key| msg.size % key == 0 }
+    valid_size.each do |key|
+      a =  { key.to_s => decrypt(msg, key) }
+      save_file(dir, name, key.to_s, a[key.to_s])
+      puts a
+    end
+  end
+  :private
+  def save_file(path, prefix, sufix, data)
+    File.open(path + prefix + '_' + sufix, 'wb').write(data)
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
-# image_path = File.new('', rb)
-  # cipher = Scytale.new
+  #name = File.expand_path('test.txt')
+  msg = File.open('test.txt', 'rb').read
+  cipher = Scytale.new.encrypt(msg, 1027)
+  #puts cipher
+  #puts Scytale.new.decrypt(cipher, 4)
+  Scytale.new.hack(cipher)
 end
