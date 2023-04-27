b8a482f6a43168e81ab938ea5ef44f5f0228e17e
3rickDJ <erickdejesusmolina@gmail.com>
2023-04-27 11:24:08 -0600

    update image

--- lib/escitala.rb
+++ lib/escitala.rb
@@ -8,18 +8,18 @@ class Scytale
     msg.chars.each_slice(key).to_a.transpose.join
   end
 
-  def hack(msg, name='file', dir='files/')
-    valid_size = (2...msg.size).filter { |key| msg.size % key == 0 }
-    valid_size.each do |key|
-      save_file(dir,  key.to_s, name, decrypt(msg, key))
-    end
-  end
-
   def hack_puts(msg)
     puts "\n\nPossible messages:"
     valid_size = (2..(msg.size-1)).filter { |key| msg.size % key == 0 }
     valid_size.each do |key|
       puts "💌'#{decrypt(msg, key)}' 🔑 #{key}"
+    end
+  end
+
+  def hack(msg, name='file', dir='files/')
+    valid_size = (2...msg.size).filter { |key| msg.size % key == 0 }
+    valid_size.each do |key|
+      save_file(dir,  key.to_s, name, decrypt(msg, key))
     end
   end
 
@@ -35,16 +35,16 @@ end
 
 
 if $PROGRAM_NAME == __FILE__
-  name = File.expand_path('test.txt')
-  msg = data('img.jpg')
-  cipher = Scytale.new.encrypt(msg, 1027)
-  #input = "la vaca lola tiene cabeza y tiene cola😏 y hace muuuu"
-  #cipher = Scytale.new.encrypt(input, 18)
-  #File.open('ciphered.jpg', 'wb').write(cipher)
-  #data = File.open('ciphered.jpg', 'rb').read
-  Scytale.new.hack(cipher)
-  #Scytale.new.hack(cipher, name='.jpg', dir='imgs/')
-  #name = File.open('test.txt', 'rb').read
-  #cipher = Scytale.new.encrypt(name, 1291)
-  #Scytale.new.hack(cipher, name='.txt', dir='files/')
+  img = File.open('imagen.jpg', 'rb').read
+  cipher = Scytale.new.encrypt(img, 90)
+  File.open('ciphered.jpg', 'wb').write(cipher)
+  # Guardar imagen cifrada
+  data = File.open('ciphered.jpg', 'rb').read
+  Scytale.new.hack(data, name='XXXXXimg.txt', dir='imgs/')
+  data = data('texto_original.txt')
+  cipher = Scytale.new.encrypt(data, 109)
+  File.open('texto_ciphered.txt', 'wb').write(cipher)
+  data = data('texto_ciphered.txt')
+  Scytale.new.hack(data, name='texto_desencriptado.txt', dir='files/')
 end
+
